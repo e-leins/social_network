10e83cfe520a4cb8d1d07387aeef4a2450088e1e
e-leins <rabota.leins@gmail.com>
2020-05-31 15:57:55 +0300

    Initial commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+ export function Profile() {
+     return (
+         <div className='content'>
+            <div className ="photo"><img
+                 src="https://cdn.pixabay.com/photo/2020/05/08/19/09/daisy-flower-5147010_1280.jpg"/> </div>
+             <div>
+                 <div> ava + description</div>
+                 <div>My posts</div>
+                 <div>New post</div>
+                 <div>post1</div>
+                 <div>post2</div>
+             </div>
+         </div>
+     )
+ }
